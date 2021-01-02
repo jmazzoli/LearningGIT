8873ceaa07281b24ddceae691d82954e0f0889a0
Jorge Mazzoli <jmazzoli@wamnet.com.ar>
2021-01-02 12:19:24 -0300

    inicio

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "zcm");
+if ($mysqli->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "\n";
+
+$mysqli = new mysqli("127.0.0.1", "root", "", "zcm", 3306);
+if ($mysqli->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+echo $mysqli->host_info . "\n";
+?>
